c7e94c5967965a58b11a46746b43e66b41aac7ac
Dylan Waldner <dylantw15@login1.ls6.tacc.utexas.edu>
2024-11-20 13:18:58 -0600

    Remove *.sub *.sh files

--- device.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ~/.bashrc
-source /lusr/opt/miniconda/bin/activate pytorch-cuda
-
-# Check if app.py exists
-python3 device.py
--- test_bnn_neat_full.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /lusr/opt/miniconda/bin/activate pytorch-cuda
-# Install pytest
-pip install pytest
-
-# Run the Python test
-python3 test_bnn_neat_full.py
-
